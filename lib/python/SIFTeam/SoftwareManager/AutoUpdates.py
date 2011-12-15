a04f15f782432d45bcccd070cf5db3f2be3a2d0f
skaman <sandro@skanetwork.com>
2011-12-15 21:58:33 +0100

    Fix software automatic updates

--- lib/python/SIFTeam/SoftwareManager/AutoUpdates.py
+++ lib/python/SIFTeam/SoftwareManager/AutoUpdates.py
@@ -2,10 +2,10 @@ from Components.config import config
 
 from Stack import SMStack, smstack
 
-def doAutomaticUpdates():
+def doAutomaticSoftwareUpdates():
 	if len(smstack.upgradables) > 0:
 		smstack.add(SMStack.UPGRADE, "auto")
 	
-def startAutomatiUpdates(session, id, arg):
+def startAutomaticSoftwareUpdates(session, id, arg):
 	if config.sifteam.cloud.softwareupdates.value:
-		smstack.add(SMStack.UPDATE, "", doAutomaticUpdates)
+		smstack.add(SMStack.UPDATE, "", doAutomaticSoftwareUpdates)
