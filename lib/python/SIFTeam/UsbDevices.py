542e2d547703a4ee5e27550eb47585357ba72fe0
skaman <sandro@skanetwork.com>
2012-01-19 04:31:37 +0100

    Complete usb devices panel

--- lib/python/SIFTeam/UsbDevices.py
+++ lib/python/SIFTeam/UsbDevices.py
@@ -131,6 +131,15 @@ class UsbDeviceAutoInstall:
 		if result == 0:
 			self.device.setStatus(3)	# mark as installed
 			smstack.add(SMStack.INSTALL_WITH_REBOOT, self.device.getPackage())
+			
+			# if necessary open usbdevices view
+			found = False
+			for dialog in self.session.dialog_stack:
+				if isinstance(dialog, UsbDevices):
+					found = True
+					break
+			if not found:
+				self.session.open(UsbDevices)
 		elif result == 1:
 			self.device.setStatus(1)	# mark as "ask again"
 		elif result == 2:
@@ -204,13 +213,15 @@ class UsbDevicesProbe:
 		[57005, 48879], # BRCM OHCI USB2.0
 	]
 	
-	def __init__(self, session):
+	def __init__(self, session, auto = False):
 		self.session = session
+		self.auto = auto
 		self.installstack = []
-		self.timer = eTimer()
-		self.timer.callback.append(self.probe)
-		print "START TIMER!!!"
-		self.timer.start(10000, 1) # delay of 10 seconds
+		self.devices = []
+		if self.auto:
+			self.timer = eTimer()
+			self.timer.callback.append(self.probe)
+			self.timer.start(10000, 1) # delay of 10 seconds
 		
 	def installComplete(self, devinst):
 		self.installstack.remove(devinst)
@@ -221,10 +232,9 @@ class UsbDevicesProbe:
 				return True
 				
 	def probe(self):
-		if len(self.session.dialog_stack) > 0:
+		if len(self.session.dialog_stack) > 0 and self.auto:
 			# osd busy.. delay of 10 seconds
 			self.timer.start(10000, 1) # delay of 20 seconds
-			print "delay"
 			return
 			
 		devdirs = os.listdir("/sys/bus/usb/devices/")
@@ -240,25 +250,30 @@ class UsbDevicesProbe:
 					continue
 					
 				device = UsbDevice(idVendor, idProduct, product)
-				devinst = UsbDeviceAutoInstall(self.session, device)
-				self.installstack.append(devinst)
-				devinst.installCb(self.installComplete)
-				devinst.install()
+				if self.auto:
+					devinst = UsbDeviceAutoInstall(self.session, device)
+					self.installstack.append(devinst)
+					devinst.installCb(self.installComplete)
+					devinst.install()
+				else:
+					self.devices.append(device)
+					
 			except Exception, e:
 				pass
 				
-		global deviceprobe
-		deviceprobe = None
+		if self.auto:
+			global deviceprobe
+			deviceprobe = None
 				
 devicenotifier = None
 deviceprobe = None
 def initUsbNotifier(session):
 	global devicenotifier, deviceprobe
 	devicenotifier = UsbDevicesNotifier(session)
-	deviceprobe = UsbDevicesProbe(session)
+	deviceprobe = UsbDevicesProbe(session, True)
 	
-def TunerEntry(name, module, started):
-	if started:
+def TunerEntry(name, module, installed):
+	if installed:
 		picture = LoadPixmap(cached = True, path = resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/icons/lock_on.png"));
 	else:
 		picture = LoadPixmap(cached = True, path = resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/icons/lock_off.png"));
@@ -269,101 +284,90 @@ class UsbDevices(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		
+		self.index = 0
+		
 		self["menu"] = List(list())
 		self["menu"].onSelectionChanged.append(self.selectionChanged)
 		self["key_green"] = Button("")
 		self["key_red"] = Button("")
-		self["key_blue"] = Button(_("Exit"))
+		self["key_blue"] = Button("")
 		self["key_yellow"] = Button("")
 				
 		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
 				{
-					"red": self.ok,
-					#"green": self.green,
-					"blue": self.close,
-					"cancel": self.close,
+					"red": self.install,
+					"cancel": self.close
 				}, -2)
 	
-		#lsusb = os.popen("/sbin/lsusb")
-		#buff = lsusb.read()
-		#lsusb.close()
-		#buff = buff.split("\n")
-		buff = []
-		i = 0
-		self.usblist = []
-		while i < len(buff):
-			if len(buff[i]) > 0:
-				offset = buff[i].find(": ID ") + 5
-				entry = buff[i][offset:]
-				if entry[:9] != "0000:0000":
-					self.usblist.append([entry[0:4], entry[5:9], entry[10:].strip()])
-			i += 1
-			
-		#dict = open("/usr/share/usbloader/dictionary.csv", "r")
-		self.usbdict = []
-		#for line in dict:
-		#	buff = line.strip().split(";")
-		#	
-		#	buff[0] = buff[0][2:]
-		#	if len(buff[0]) == 1:
-		#		buff[0] = "000" + buff[0]
-		#	elif len(buff[0]) == 2:
-		#		buff[0] = "00" + buff[0]
-		#	elif len(buff[0]) == 3:
-		#		buff[0] = "0" + buff[0]
-		#		
-		#	buff[1] = buff[1][2:]
-		#	if len(buff[1]) == 1:
-		#		buff[1] = "000" + buff[1]
-		#	elif len(buff[1]) == 2:
-		#		buff[1] = "00" + buff[1]
-		#	elif len(buff[1]) == 3:
-		#		buff[1] = "0" + buff[1]
-		#	
-		#	self.usbdict.append(buff)
-		#dict.close()
+		devprobe = UsbDevicesProbe(session)
+		devprobe.probe()
+		self.devices = devprobe.devices
 
-		modules = open("/proc/modules", "r")
-		self.moduleslist = []
-		for line in modules:
-			buff = line.strip().split(" ")
-			buff = buff[0].strip()
-			self.moduleslist.append(buff)
-		modules.close()
+		self.drawList()
+		self.selectionChanged()
 		
+		smstack.callbacks.append(self.drawList)
+		
+	def install(self):
+		if len(self.devices) == 0:
+			return
+			
+		index = self["menu"].getIndex()
+		if index == None:
+			index = 0
+		
+		if len(self.devices[index].getPackage()) == 0 or smstack.checkIfPending(self.devices[index].getPackage()):
+			return
+			
+		if fileExists("/usr/lib/opkg/info/%s.control" % self.devices[index].getPackage()):
+			smstack.add(SMStack.REMOVE, self.devices[index].getPackage())
+			self.devices[index].setStatus(2)
+		else:
+			smstack.add(SMStack.INSTALL_WITH_REBOOT, self.devices[index].getPackage())
+			self.devices[index].setStatus(3)
+			
 		self.drawList()
 		
-	def ok(self):
-		#global devicenotifier
-		#devicenotifier.newDevice(3277, 147)
-		pass
-		
 	def selectionChanged(self):
-		pass
-
-	def searchDevice(self, vid, pid):
-		for entry in self.usbdict:
-			if entry[0] == vid and entry[1] == pid:
-				return entry
-				
-		return None
-		
-	def isInModules(self, name):
-		for entry in self.moduleslist:
-			if entry == name:
-				return True
-			elif entry == name.replace("-", "_"):
-				return True
+		if len(self.devices) == 0:
+			return
 			
-		return False
-	
+		index = self["menu"].getIndex()
+		if index == None:
+			index = 0
+		
+		self.index = index
+		
+		if len(self.devices[index].getPackage()) == 0 or smstack.checkIfPending(self.devices[index].getPackage()):
+			self["key_red"].setText("")
+		elif fileExists("/usr/lib/opkg/info/%s.control" % self.devices[index].getPackage()):
+			self["key_red"].setText(_("Remove"))
+		else:
+			self["key_red"].setText(_("Install"))
+			
 	def drawList(self):
-		llist = []
-		for entry in self.usblist:
-			dentry = self.searchDevice(entry[0], entry[1])
-			if (dentry == None):
-				llist.append(TunerEntry(entry[0] + ":" + entry[1] + " - " + entry[2], "Device not in dictionary", False))
+		list = []
+		count = 0
+		for device in self.devices:
+			if len(device.getPackage()) > 0:
+				inprogress = smstack.checkIfPending(device.getPackage())
+				if inprogress:
+					list.append(TunerEntry(device.getDescription(), smstack.getMessage(device.getPackage()), False))
+				else:
+					installed = fileExists("/usr/lib/opkg/info/%s.control" % device.getPackage())
+					if installed:
+						info = device.getPackage() + " - installed"
+					else:
+						info = device.getPackage() + " - not installed"
+					list.append(TunerEntry(device.getDescription(), info, installed))
 			else:
-				llist.append(TunerEntry(entry[0] + ":" + entry[1] + " - " + entry[2], dentry[2] + ": " + dentry[3] + " (driver: " + dentry[4] + ")", self.isInModules(dentry[4])))
-
-		self["menu"].setList(llist)
+				list.append(TunerEntry(device.getDescription(), "Driver not found", False))
+				
+			count += 1
+			
+		self["menu"].setList(list)
+		self["menu"].setIndex(self.index)
+		
+	def quit(self):
+		smstack.callbacks.remove(self.drawList)
+		self.close()
