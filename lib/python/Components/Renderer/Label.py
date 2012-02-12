36fcf9f60e1c86b0dd9fccd74ca275b7426bcc86
skaman <sandro@skanetwork.com>
2012-02-12 11:50:28 +0100

    Fix label renderer to support static text from skin

--- lib/python/Components/Renderer/Label.py
+++ lib/python/Components/Renderer/Label.py
@@ -7,7 +7,18 @@ class Label(VariableText, Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
-
+		self.forced = False
+		
+	def applySkin(self, desktop, parent):
+		attribs = []
+		for (attrib, what,) in self.skinAttributes:
+			if (attrib == 'text'):
+				self.setText(what)
+				self.forced=True
+			else:
+				attribs.append((attrib, what))
+		return Renderer.applySkin(self, desktop, parent)
+		
 	GUI_WIDGET = eLabel
 
 	def connect(self, source):
@@ -15,8 +26,15 @@ class Label(VariableText, Renderer):
 		self.changed((self.CHANGED_DEFAULT,))
 
 	def changed(self, what):
-		if what[0] == self.CHANGED_CLEAR:
-			self.text = ""
-		else:
-			self.text = self.source.text
+		if self.forced:
+			return
+			
+		# it could be called before the applyskin
+		try:
+			if what[0] == self.CHANGED_CLEAR:
+				self.text = ""
+			else:
+				self.text = self.source.text
+		except Exception, e:
+			pass
 
